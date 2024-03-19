038b8f8524e1037f3f0e6573038c74db6de7d3da
hamin <hamin@bisonai.com>
2024-03-19 10:45:30 +0900

    Add ContractController endpoint to retrieve contract information

---
+++ module-open-api/src/main/kotlin/io/klaytn/finder/interfaces/rest/api/view/ContractItemView.kt
@@ -0,0 +1,35 @@
+package io.klaytn.finder.interfaces.rest.api.view
+
+import io.klaytn.finder.domain.common.ContractType
+import io.swagger.v3.oas.annotations.media.Schema
+import java.math.BigDecimal
+
+@Schema
+data class ContractItemView(
+    @Schema(title="Type")
+    val type: ContractType,
+
+    @Schema(title="Name")
+    val name: String?,
+
+    @Schema(title="Symbol")
+    val symbol: String?,
+
+    @Schema(title="Decimal")
+    val decimal: Int,
+
+    @Schema(title="Total Supply")
+    val totalSupply: BigDecimal,
+
+    @Schema(title="Token Image")
+    val icon: String?,
+
+    @Schema(title="Official Site URL")
+    val officialSite: String?,
+
+    @Schema(title="Official Email Address")
+    val officialEmailAddress: String?,
+
+    @Schema(title="Verification Status")
+    val verified: Boolean,
+)
---
+++ module-open-api/src/main/kotlin/io/klaytn/finder/interfaces/rest/api/view/mapper/ContractToItemViewMapper.kt
@@ -0,0 +1,24 @@
+package io.klaytn.finder.interfaces.rest.api.view.mapper
+
+import io.klaytn.commons.model.mapper.Mapper
+import io.klaytn.finder.domain.mysql.set1.Contract
+import io.klaytn.finder.interfaces.rest.api.view.ContractItemView
+import org.springframework.stereotype.Component
+
+@Component
+class ContractToItemViewMapper(
+) : Mapper<Contract, ContractItemView> {
+    override fun transform(source: Contract): ContractItemView {
+        return ContractItemView(
+            type = source.contractType,
+            name = source.name,
+            symbol = source.symbol,
+            decimal = source.decimal,
+            totalSupply = source.totalSupply,
+            icon = source.icon,
+            officialSite = source.officialSite,
+            officialEmailAddress = source.officialEmailAddress,
+            verified = source.verified,
+        )
+    }
+}
