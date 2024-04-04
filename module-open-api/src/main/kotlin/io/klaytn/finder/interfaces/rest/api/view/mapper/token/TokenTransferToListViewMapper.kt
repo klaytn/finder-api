38d2f33f79552c1cad1942f1cf1048b24a4e676a
hamin <hamin@bisonai.com>
2024-04-04 11:12:11 +0900

    Add transaction index to TokenTransferListView

--- module-open-api/src/main/kotlin/io/klaytn/finder/interfaces/rest/api/view/mapper/token/TokenTransferToListViewMapper.kt
+++ module-open-api/src/main/kotlin/io/klaytn/finder/interfaces/rest/api/view/mapper/token/TokenTransferToListViewMapper.kt
@@ -7,17 +7,21 @@ import io.klaytn.finder.infra.utils.applyDecimal
 import io.klaytn.finder.interfaces.rest.api.view.model.ContractSummary
 import io.klaytn.finder.interfaces.rest.api.view.model.token.TokenTransferListView
 import io.klaytn.finder.service.ContractService
+import io.klaytn.finder.service.TransactionService
 import org.springframework.stereotype.Component
 
 @Component
 class TokenTransferToListViewMapper(
     private val contractService: ContractService,
+    private val transactionService: TransactionService
 ) : ListMapper<TokenTransfer, TokenTransferListView> {
     override fun transform(source: List<TokenTransfer>): List<TokenTransferListView> {
         val contractMap = contractService.getContractMap(source.map { it.contractAddress }.toSet())
+        val transactionMap = transactionService.getTransactionByHashes(source.map { it.transactionHash }).associateBy { it.transactionHash }
 
         return source.map { tokenTransfer ->
             val contract = contractMap[tokenTransfer.contractAddress]
+            val transaction = transactionMap[tokenTransfer.transactionHash]
             val contractSummary = ContractSummary.of(tokenTransfer.contractAddress, contract)
             val amount = contract?.let { tokenTransfer.amount.toBigDecimal().applyDecimal(it.decimal) }
                 ?: tokenTransfer.amount.toBigDecimal()
@@ -26,6 +30,7 @@ class TokenTransferToListViewMapper(
                 contract = contractSummary,
                 blockId = tokenTransfer.blockNumber,
                 transactionHash = tokenTransfer.transactionHash,
+                transactionIndex = transaction?.transactionIndex,
                 datetime = DateUtils.from(tokenTransfer.timestamp),
                 from = tokenTransfer.from.address,
                 to = tokenTransfer.to?.address,
--- module-open-api/src/main/kotlin/io/klaytn/finder/interfaces/rest/api/view/model/token/TokenTransferListView.kt
+++ module-open-api/src/main/kotlin/io/klaytn/finder/interfaces/rest/api/view/model/token/TokenTransferListView.kt
@@ -16,6 +16,9 @@ data class TokenTransferListView(
     @Schema(title = "Transaction Hash")
     val transactionHash: String,
 
+    @Schema(title = "Transaction Index")
+    val transactionIndex: Int?,
+
     @Schema(title = "Transaction Time")
     val datetime: Date,
 
