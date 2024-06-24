8192b1d9d8fc186ea812f7925e92eec2144d06a2
jayce <jayce@bisonai.com>
2024-06-24 17:37:06 +0900

    chore: prettier

--- module-front-api/src/main/kotlin/io/klaytn/finder/service/GovernanceCouncilInfoService.kt
+++ module-front-api/src/main/kotlin/io/klaytn/finder/service/GovernanceCouncilInfoService.kt
@@ -5,16 +5,8 @@ import io.klaytn.commons.utils.logback.logger
 import io.klaytn.commons.utils.retrofit2.orElseThrow
 import io.klaytn.finder.config.ClientProperties
 import io.klaytn.finder.domain.common.GovernanceCouncilContractType
-import io.klaytn.finder.domain.mysql.set4.GovernanceCouncilsInfo
-import io.klaytn.finder.domain.mysql.set4.GovernanceCouncilCategories
-import io.klaytn.finder.domain.mysql.set4.GovernanceCouncilCommunities
-import io.klaytn.finder.domain.mysql.set4.GovernanceCouncilContracts
-import io.klaytn.finder.domain.mysql.set4.GovernanceCouncilsInfoRepository
-import io.klaytn.finder.domain.mysql.set4.GovernanceCouncilCategoriesRepository
-import io.klaytn.finder.domain.mysql.set4.GovernanceCouncilCommunitiesRepository
-import io.klaytn.finder.domain.mysql.set4.GovernanceCouncilContractsRepository
+import io.klaytn.finder.domain.mysql.set4.*
 import io.klaytn.finder.infra.client.KaiaSquareClient
-import io.klaytn.finder.infra.client.KaiaSquareGovernanceCouncilCommunity
 import io.klaytn.finder.infra.client.KaiaSquareGovernanceCouncilCommunityLink
 import io.klaytn.finder.infra.db.DbConstants
 import io.klaytn.finder.infra.utils.DateUtils
@@ -298,9 +290,15 @@ class GovernanceCouncilInfoService(
     fun getAll(): List<GovernanceCouncilWithCategoryView> {
         try {
             val governanceCouncilInfo: List<GovernanceCouncilsInfo> = governanceCouncilsInfoRepository.findAll()
-            val governanceCouncilCategories: List<GovernanceCouncilCategories> = governanceCouncilCategoriesRepository.findAll()
+            val governanceCouncilCategories: List<GovernanceCouncilCategories> =
+                governanceCouncilCategoriesRepository.findAll()
 
-            return GovernanceCouncilToListViewMapper().transform(Pair(governanceCouncilInfo, governanceCouncilCategories))
+            return GovernanceCouncilToListViewMapper().transform(
+                Pair(
+                    governanceCouncilInfo,
+                    governanceCouncilCategories
+                )
+            )
         } catch (e: Exception) {
             logger.error("Failed to fetch governance council data", e)
             throw e
